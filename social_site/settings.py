8f091f80373255db0cc22e76445201dd5d1c9055
mat0ccdeekk <mattia.lavecchia@gmail.com>
2020-10-26 17:25:05 +0100

    make it better

--- social_site/settings.py
+++ social_site/settings.py
@@ -74,7 +74,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.storage.CompressedManifestStaticFilesStorage',
 ]
 
 ROOT_URLCONF = 'social_site.urls'
