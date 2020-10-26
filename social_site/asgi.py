cd99a39b1608e171350217d5246972e530a69170
mat0ccdeekk <mattia.lavecchia@gmail.com>
2020-10-26 21:37:24 +0100

    make it better

--- social_site/asgi.py
+++ social_site/asgi.py
@@ -10,7 +10,3 @@ from channels.routing import get_default_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "social_site.settings")
 django.setup()
 application = get_default_application()
-
-
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
